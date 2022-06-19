f2af2cc55ec8eb8e689cd37af4e0384f19221373
Pedro Cauan <90540372+pedrocauan@users.noreply.github.com>
2022-06-19 19:48:59 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 const menu = document.querySelector("#menu");
 const burger = document.querySelector("#menu-burg"); //itens do menu
 const links = document.querySelectorAll("a");
+const html = document.querySelector("html"); /*impede q o usuario role a tela quando tiver no menu*/
 
 burger.addEventListener("click", () => {
     menu.classList.toggle("open");
+    html.classList.toggle("open");
 });
 // Sai do menu quando clica em alguma das opções
 links.forEach(link => {
